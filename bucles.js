f36c358a93567f4be71e9a69d5486a4043cd8e28
paula1910 <118273636+paula1910@users.noreply.github.com>
2022-11-22 13:14:11 +0100

    Add files via upload

---
+++ bucles.js
@@ -0,0 +1,14 @@
+
+function setup(){
+    createCanvas(500,200)
+    //RGB
+    background(125);
+}
+function draw(){
+    var x = 50;
+
+    while(x<width-25){
+        ellipse(x, height/2, 25,25);
+        x = x + 50;
+    }
+}
